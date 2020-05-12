f516edbbcb5cadf5e36465b590acbcbca38e5223
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-12 18:00:41 -0500

    fixed typo

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -75,8 +75,8 @@ class DoublyLinkedList:
         self.delete(self.head)
         return value
 
- """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
@@ -89,9 +89,7 @@ class DoublyLinkedList:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
-
-
- """Removes the List's current tail node, making the 
+    """Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
 
@@ -100,7 +98,7 @@ class DoublyLinkedList:
         self.delete(self.tail)
         return value
 
-"""Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
@@ -110,8 +108,7 @@ class DoublyLinkedList:
         self.delete(node)
         self.add_to_head(value)
 
-
- """Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
@@ -121,7 +118,7 @@ class DoublyLinkedList:
         self.delete(node)
         self.add_to_tail(value)
 
-"""Removes a node from the list and handles cases where
+    """Removes a node from the list and handles cases where
     the node was the head or the tail"""
 
     def delete(self, node):
