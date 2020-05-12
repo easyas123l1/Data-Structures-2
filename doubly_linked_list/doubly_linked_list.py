281daad50cb65b8b666d32515a3aea3115acf793
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-12 17:59:15 -0500

    complete move_to_end

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -109,3 +109,14 @@ class DoublyLinkedList:
         value = node.value
         self.delete(node)
         self.add_to_head(value)
+
+
+ """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+
+    def move_to_end(self, node):
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
